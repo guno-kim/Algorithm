307802bdd85a469d6b7dae33289de222ec993775
guno-kim <guno98@naver.com>
2021-05-31 23:59:39 +0900

    @:다시 풀만한 문제

--- 222.py
+++
@@ -1,3 +0,0 @@
-print(1|0)
-print(2|1)
-print(2|1)
--- a.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-class Main{
-    public static void main(String[]b){
-    Scanner a=new Scanner(System.in);
-    System.out.print(a.nextInt()-a.nextInt());
-    }
-}
