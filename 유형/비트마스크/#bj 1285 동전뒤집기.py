a5b4f17ac0918031f3e3b69f5e8e270bf9b06bc9
guno-kim <guno98@naver.com>
2021-06-04 15:48:28 +0900

    Daily problem

--- 유형/비트마스크/#bj 1285 동전뒤집기.py
+++ 유형/비트마스크/#bj 1285 동전뒤집기.py
@@ -1,5 +1,4 @@
 import sys
-import copy
 input=sys.stdin.readline
 
 n,answer=int(input()),10000
@@ -8,15 +7,15 @@ coins=list(map(lambda coin:sum(1<<(n-i-1)
     for i in range(n) if coin[i]=='T'),coins))
 
 for r in range(1<<n):
-    now_coins=copy.deepcopy(coins)
     now_sum=0
-    for i in range(n):
-        now_coins[i]^=r
 
     for i in range(n):
-        temp=bin(now_coins[i]).count('1')
+        temp=bin(coins[i]^r).count('1')
         now_sum+=min(temp,n-temp)
     answer=min(answer,now_sum)
 
 print(answer)
-        
+
+# 뒤집을 열 조합을 정해놓은 후 
+# 각 행마다 최소 T개수 찾기
+# 이 과정 모든 열 조합에서 반복
